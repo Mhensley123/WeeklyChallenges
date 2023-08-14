81edd77224df1f743c6682a2def9c4295f8542b3
Maria Hensley <mscelie34@zoho.com>
2023-08-13 19:41:50 -0500

    weekly challenges 5 and 6

--- ChallengesWithTestsMark8/ChallengesSet05.cs
+++ ChallengesWithTestsMark8/ChallengesSet05.cs
@@ -7,27 +7,107 @@ namespace ChallengesWithTestsMark8
     {
         public int GetNextNumberDivisibleByN(int startNumber, int n)
         {
-            throw new NotImplementedException();
-        }
+            if (startNumber == 0)
+            {
+                return n;
+            }
 
-        public void ChangeNamesOfBusinessesWithNoRevenueTo_CLOSED(Business[] businesses)
-        {
-            throw new NotImplementedException();
-        }
+            List<int> firstlist = new List<int>();
+            int maxLoop = startNumber * n;
+            int[] numbersDivisiblebyN = new int[0];
+            var result = 0;
 
-        public bool IsAscendingOrder(int[] numbers)
-        {
-            throw new NotImplementedException();
+            for (int i = startNumber; i < maxLoop; i++)
+            {
+
+                if (i > startNumber && i % n == 0)
+                {
+                    result = i;
+                    break;
+                }
+            }
+            return result;
         }
+    }
+
+    public void ChangeNamesOfBusinessesWithNoRevenueTo_CLOSED(Business[] businesses)
+    {
+        for (int i = 0; i < businesses.Length; i++)
+        {
+            if (businesses[i].TotalRevenue == 0)
+            {
+                businesses[i].Name = "CLOSED";
+            }
+        }
+    }
+
+    public bool IsAscendingOrder(int[] numbers)
+    {
+        if (numbers == null || numbers.Length == 0)
+        {
+            return false;
+        }
+        for (int i = 1; i < numbers.Length; i++)
+        {
+
+            if (numbers[i] < numbers[i - 1])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+            
+    
 
         public int SumElementsThatFollowAnEven(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null || numbers.Length == 0)
+        {
+            return 0;
+        }
+        int sum = 0;
+
+        for (int i = 1; i < numbers.Length; i++)
+        {
+            if (numbers[i - 1] % 2 == 0)
+            {
+                sum += numbers[i];
+            }
+        }
+            return sum;
         }
 
         public string TurnWordsIntoSentence(string[] words)
         {
-            throw new NotImplementedException();
+            if (words == null || words.Length == 0)
+            {
+                return "";
+            }
+        
+        int goodwords = 0;
+        List<string> wordlist = new List<string>();
+
+        for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i] == "" || words[i] == " " || words[i] == " ")
+                {
+               
+                }
+                else
+                    {
+                    wordlist.Add(words[i].Trim());
+                    goodwords += 1;
+                    }
+            }
+                if (goodwords == 0)
+        {
+            return "";
+        }
+                else
+         {
+            return String.Join(" ", wordlist) + ".";
+        }
         }
 
         public double[] GetEveryFourthElement(List<double> elements)
